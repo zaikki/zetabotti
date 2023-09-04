7ff2b37a9a7fe78bd68ca3538083978913ac9520
Lauri Saarelainen (c947971) <lauri.saarelainen@op.fi>
2023-09-04 16:02:27 +0300

    General Removed unused code and fixed some API response errors if Spotify was is not running

--- scripts/spotify_token_fetcher.py
+++ scripts/spotify_token_fetcher.py
@@ -7,16 +7,16 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-client_id=os.getenv('SPOTIFY_CLIENT_ID')
-client_secret=os.getenv('SPOTIFY_CLIENT_SECRET')
-callback_uri="http://localhost:7777/callback"
-spotify_authorization_code=os.getenv("SPOTIFY_AUTHORIZATION_TOKEN")
+client_id = os.getenv("SPOTIFY_CLIENT_ID")
+client_secret = os.getenv("SPOTIFY_CLIENT_SECRET")
+callback_uri = "http://localhost:7777/callback"
+spotify_authorization_code = os.getenv("SPOTIFY_AUTHORIZATION_TOKEN")
 
 auth_headers = {
     "client_id": client_id,
     "response_type": "code",
     "redirect_uri": callback_uri,
-    "scope": "user-library-read user-read-currently-playing user-modify-playback-state"
+    "scope": "user-library-read user-read-currently-playing user-modify-playback-state",
 }
 
 
@@ -24,12 +24,13 @@ def spotify_generate_access_token():
     """THIS GENERATES ACCESS TOKEN"""
     # webbrowser.open("https://accounts.spotify.com/authorize?" + urlencode(auth_headers))
 
-
-    encoded_credentials = base64.b64encode(client_id.encode() + b':' + client_secret.encode()).decode("utf-8")
+    encoded_credentials = base64.b64encode(
+        client_id.encode() + b":" + client_secret.encode()
+    ).decode("utf-8")
 
     token_headers = {
         "Authorization": "Basic " + encoded_credentials,
-        "Content-Type": "application/x-www-form-urlencoded"
+        "Content-Type": "application/x-www-form-urlencoded",
     }
 
     token_data = {
@@ -38,10 +39,11 @@ def spotify_generate_access_token():
         "redirect_uri": callback_uri,
     }
 
-    r = requests.post("https://accounts.spotify.com/api/token", data=token_data, headers=token_headers)
-    #token = r.json()["access_token"]
+    r = requests.post(
+        "https://accounts.spotify.com/api/token", data=token_data, headers=token_headers
+    )
+    # token = r.json()["access_token"]
     print(r.json())
 
-    
 
 spotify_generate_access_token()
