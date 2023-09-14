c91bb6318d8c1bd77ff15f459b0e37b40bcc3a38
zaikki <lauri.saarelainen@zaikki.com>
2023-09-14 15:02:18 +0300

    General Use json instead of yaml for config file

--- twitch/config.py
+++ twitch/config.py
@@ -1,4 +1,5 @@
 import yaml
+import json
 
 cfg = None
 
@@ -6,6 +7,8 @@ cfg = None
 def load_config():
     global cfg
     if not cfg:
-        with open("twitch/config.yml", "r") as ymlfile:
-            cfg = yaml.unsafe_load(ymlfile)
+        # with open("twitch/config.yml", "r") as ymlfile:
+        #     cfg = yaml.unsafe_load(ymlfile)
+        with open("twitch/config.json", "r") as json_file:
+            cfg = json.load(json_file)
     return cfg
