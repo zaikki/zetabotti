cc7e3e5e7a1e098f45665fb184cae747489e64bd
zaikki <lauri.saarelainen@zaikki.com>
2023-09-14 14:50:07 +0300

    General Use unsafe_load

--- twitch/config.py
+++ twitch/config.py
@@ -7,5 +7,5 @@ def load_config():
     global cfg
     if not cfg:
         with open("twitch/config.yml", "r") as ymlfile:
-            cfg = yaml.safe_load(ymlfile)
+            cfg = yaml.unsafe_load(ymlfile)
     return cfg
